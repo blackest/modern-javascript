1cbb8e9248c0cc79a614d996e032ff2b4a16b97c
blackest <blackest@users.noreply.github.com>
2024-07-17 02:12:12 +0100

    test2

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -10,3 +10,6 @@ console.log(!true);
 console.log(!false);
 
 // this is a comment#
+const request = new XMLHttpRequest();
+request.open('GET' , 'https://www.smartgriddashboard.com/DashboardService.svc/stats?area=windstats&region=ALL&datefrom=16-Jul-2024+00%3A00&dateto=16-Jul-2024+23%3A59');
+request.send();
