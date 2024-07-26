9a6302d982e2ec817ce09f2cf14892dc9e67671b
blackest <blackest@users.noreply.github.com>
2024-07-26 05:07:27 +0100

    try3
    updated comments

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,16 +1,16 @@
 // variables & block scope
-let age = 30;
+let age = 30;  //Global scope
 
 if(true){
 
   // age = 40;
-  let age = 40;
+  let age = 40;   // local scope
   let name = 'shaun';
   console.log('inside 1st code block:', age, name);
 
   if(true){
 
-    let age = 50;
+    let age = 50; // local scope
     console.log('inside 2nd code block:', age, name);
 
   }
